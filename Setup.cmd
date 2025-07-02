3e65b0b5251d6083c610167cb9c07e70be62cc08
Martin Baute <solar@rootdirectory.de>
2025-07-02 23:16:37 +0200

    Environment Check

--- Setup.cmd
+++ Setup.cmd
@@ -4,4 +4,23 @@ PowerShell -ExecutionPolicy Bypass -NoProfile -NoLogo "iex (${%~f0} | out-string
 Exit /b %errorlevel%
 : end batch prelude #>
 
+$rootdirs = (
+@( 'P4STREAMS', 'Perforce Streams' ),
+@( 'P4CLIENTS', 'Perforce non-stream repositories' )
+)
 
+foreach ( $arr in $rootdirs )
+{
+    $var = [Environment]::GetEnvironmentVariable( $arr[0] )
+
+    if ( $null -eq $var )
+    {
+        Write-Output "$($arr[0])=UNSET"
+    }
+    else
+    {
+        Write-Output "$($arr[0])=$var"
+    }
+}
+
+# vim: filetype=ps1
